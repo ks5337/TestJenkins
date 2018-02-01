66c4a3fb62d9958c407325719999102bd73e45ba
markGit <markGit@hotmail.com>
2018-02-01 14:23:30 +0800

    20180201-2

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -37,7 +37,7 @@ public class TestGrid {
 		//String Node = "http://selenium-hub:4444/wd/hub";
 		//String URL = "http://tores3-test.router.default.svc.cluster.local/TORES/";
 		//String URL = "http://172.17.0.8:8080/TORES/index.jsp";
-		String Node = "http://172.17.0.7:4444/wd/hub";
+		String Node = "http://172.17.0.5:4444/wd/hub";
 		
 		if (browser.equalsIgnoreCase("firefox")) {
 			System.out.println(" Executing on FireFox");
